808d0ccd7733296a493325b91cb29c2961ddc81e
GreatIrr <32968261+GreatIrr@users.noreply.github.com>
2022-05-24 18:36:24 +0300

    Update AppiumTest.py

--- lesson3/AppiumTest.py
+++ lesson3/AppiumTest.py
@@ -1,17 +1,18 @@
 from appium import webdriver
-from appium.webdriver.common.appiumby import AppiumBy
+
+from View import SettingsView
 
 print("before desired caps")
 
 desired_capabilities = {
-  "platformName": "iOS",
-  "platformVersion": "12.5.5",
-  "deviceName": "iPhone 6",
-  "app": "",
-  "udid": "98c0e343a64b454500e81ee035740c8d212e6bfb",
-  "noReset": "true",
-  "derivedDataPath": "/Users/bartnytskairyna/Git/aqa-mobile-ring/build/wda/98c0e343a64b454500e81ee035740c8d212e6bfb/WebDriverAgent",
-  "automationName": "XCuiTest"
+    "platformName": "iOS",
+    "platformVersion": "12.5.5",
+    "deviceName": "iPhone 6",
+    "app": "",
+    "udid": "98c0e343a64b454500e81ee035740c8d212e6bfb",
+    "noReset": "true",
+    "derivedDataPath": "/Users/bartnytskairyna/Git/aqa-mobile-ring/build/wda/98c0e343a64b454500e81ee035740c8d212e6bfb/WebDriverAgent",
+    "automationName": "XCuiTest"
 }
 
 print("initing driver")
@@ -21,18 +22,14 @@ driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_capabilities)
 print("finished driver")
 
 driver.activate_app("com.apple.Preferences")
+# driver.find_element_by_ios_predicate("type=='XCUIElementTypeCell' and name=='Wi-Fi'").click()
 
-locator_wifi = "type=='XCUIElementTypeCell' and name=='Wi-Fi'"
-locator_current_wifi = "type == 'XCUIElementTypeStaticText' and name !='Wi-Fi'"
-wifi_element = driver.find_element(AppiumBy.IOS_PREDICATE, locator_wifi)
-current_wifi_element = wifi_element.find_element(AppiumBy.IOS_PREDICATE, locator_current_wifi)
+settings_view = SettingsView(driver)
 
-
-wifi_element_text = current_wifi_element.text
+wifi_element_text = settings_view.get_current_wifi_text()
 
 print("i got next text " + wifi_element_text)
 
 assert "ZTE" == wifi_element_text, f"{wifi_element_text} is unexpected"
 
 driver.quit()
-
